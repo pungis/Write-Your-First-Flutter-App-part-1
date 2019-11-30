48a0dcec212edd10dc0f1cd612dc8de4369feb95
pungis <filip.bsl@gmail.com>
2019-11-30 11:32:30 +0100

    Create the initial Flutter app

---
+++ startup_name/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to flutter'),
+        ),
+        body: const Center(
+          child: const Text('Hello World'),
+        )
+      )
+    );
+  }
+}
